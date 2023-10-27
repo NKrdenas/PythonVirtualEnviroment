83e100a93d4285500da9afdedf5e3ff0a8b6be13
ncard <ncardenas101903@gmail.com>
2023-10-27 10:23:21 -0400

    Cambios funcionales en app

--- app/read_csv.py
+++ app/read_csv.py
@@ -12,5 +12,5 @@ def read_csv(path):
         return data
 
 if __name__ == '__main__':
-    data = read_csv('./app/data.csv')
-    print(data[42])
+    data = read_csv('./data.csv')
+    print(data[0])
