b00fcbae972468fd0802d65848dbdb4ca1be1ed1
ncard <ncardenas101903@gmail.com>
2023-10-30 13:57:46 -0400

    Configuracion de nuestra propia api con HTML

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,16 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return " <h1> Quiubo parceros </h1> "
 
 def run():
     store.get_categories()
