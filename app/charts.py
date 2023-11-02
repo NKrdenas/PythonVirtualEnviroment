7812fe428b8fd9f229c9bc168742ab7e8103562d
ncard <ncardenas101903@gmail.com>
2023-11-02 16:05:59 -0400

    Vinculando archivos a docker

--- app/charts.py
+++ app/charts.py
@@ -11,7 +11,7 @@ def generate_pie_chart(labels, values):
     fig, ax = plt.subplots()
     ax.pie(values, labels= labels)
     ax.axis('equal')
-    plt.savefig(f'pies/pie.png')
+    plt.savefig(f'pies/chart_pie.png')
     plt.close()
 
 if __name__ == '__main__':
